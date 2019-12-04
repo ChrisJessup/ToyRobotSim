639254a3b4f1907ad3a29a51710b9d6193771f1d
ChrisJessup <christopherjessup@hotmail.co.uk>
2019-12-04 13:47:38 +0000

    Written robot class

--- ToyRobotSim/Game.cs
+++ ToyRobotSim/Game.cs
@@ -4,9 +4,18 @@ namespace ToyRobotSim
 {
     class Game
     {
+        static int TABLE_WIDTH = 5;
+        static int TABLE_DEPTH = 5;
+        static Robot[,] Table = new Robot[TABLE_WIDTH, TABLE_DEPTH];
+        static Robot Bot = new Robot(Table);
+        enum Directions {NORTH, EAST, SOUTH, WEST};
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Input;
+            while ((Input = Console.ReadLine()) != null)
+            {
+
+            }
         }
     }
 }
--- ToyRobotSim/Robot.cs
+++ ToyRobotSim/Robot.cs
@@ -4,6 +4,90 @@ namespace ToyRobotSim
 {
     class Robot
     {
-        
+        private int X;
+        private int Y;
+        private int Facing;
+        enum Directions { NORTH, EAST, SOUTH, WEST };
+        private Robot[,] Table;
+        bool OnTable;
+
+        public Robot(Robot[,] table)
+        {
+            X = 0;
+            Y = 0;
+            Facing = 0;
+            Table = table;
+            OnTable = false;
+        }
+
+        public void Place(int x, int y, int facing)
+        {
+            if(x < Table.GetLength(0) && y < Table.GetLength(1))
+            {
+                if (x >= 0 && y >= 0)
+                {
+                    X = x;
+                    Y = y;
+                    Facing = facing;
+                    OnTable = true;
+                }
+            }
+        }
+        public void Move()
+        {
+            if (OnTable)
+            {
+                if ((Facing % 4) == (int)Directions.NORTH)
+                {
+                    if (Y < Table.GetLength(1) - 1)
+                    {
+                        Y += 1;
+                    }
+                }
+                if ((Facing % 4) == (int)Directions.EAST)
+                {
+                    if (X < Table.GetLength(0) - 1)
+                    {
+                        X += 1;
+                    }
+                }
+                if ((Facing % 4) == (int)Directions.SOUTH)
+                {
+                    if (Y > 0)
+                    {
+                        Y -= 1;
+                    }
+                }
+                if ((Facing % 4) == (int)Directions.WEST)
+                {
+                    if (X > 0)
+                    {
+                        X -= 1;
+                    }
+                }
+            }
+        }
+        public void Left()
+        {
+            if (OnTable)
+            {
+                Facing -= 1;
+            }
+        }
+        public void Right()
+        {
+            if (OnTable)
+            {
+                Facing += 1;
+            }
+        }
+        public string Report()
+        {
+            if (OnTable)
+            {
+                return X.ToString() + "," + Y.ToString() + "," + Enum.GetName(typeof(Directions), (Facing % 4));
+            }
+            return null;
+        }
     }
 }
